54222c46090b7276f970c52309c49cf680a4bf77
Junior <457030053@qq.com>
2017-07-16 12:14:53 +0800

    update Camera.h

---
+++ RayTracing/Camera.cpp
@@ -0,0 +1,63 @@
+#include"Camera.h"
+/*
+Camera类有三个坐标体系:
+Camera照相机相对于世界坐标系;
+成像平面相对于Camera的uvw坐标系;
+像素坐标相对于成像平面的二维坐标系
+(成像平面的二维坐标系是利用image_plane_corner,image_plane_uVector,image_plane_vVector构造的)
+但是实际上所有的坐标最终都是转化为世界坐标系的统一坐标.
+*/
+
+Camera::Camera(Vector3 c, Vector3 gaze, Vector3 vup, float apertureDiameter,
+	float left, float right, float top, float bottom, float dis)
+	:center(c),u0(left),v0(bottom),u1(right),v1(top),distance(dis)
+{
+	lens_radius = apertureDiameter / 2.0f;//apertureDiameter是照相机光圈的直径,除以2得到半径
+
+	uvw.initFromWV(-gaze, vup);//构造照相机坐标系
+	//gaze是光圈指向成像平面的向量,-gaze才是照相机坐标系的w分量;
+	//vup是指定的一个方向向上的向量,一般取(0,1,0)，
+    //可以通过改变vup来旋转相机从而旋转图像
+
+	image_plane_corner = center + (u0*uvw.u() + v0*uvw.v() - distance*uvw.w());
+	//成像平面左下角在世界坐标系的坐标 = 照相机中心坐标+(成像平面左下角在照相机坐标系的坐标)
+
+	image_plane_uVector = (u1 - u0)*uvw.u();
+	image_plane_vVector = (v1 - v0)*uvw.v();
+}
+Camera::Camera(const Camera& cam)
+{
+	center = cam.center;
+	image_plane_corner = cam.image_plane_corner;
+	image_plane_uVector = cam.image_plane_uVector;
+	image_plane_vVector = cam.image_plane_vVector;
+	u0 = cam.u0;
+	u1 = cam.u1;
+	v0 = cam.v0;
+	v1 = cam.v1;
+	distance = cam.distance;
+	lens_radius = cam.lens_radius;
+	uvw = cam.uvw;
+}
+
+/*
+像素位置(pixel_position_x,pixel_position_y)
+映射方法:pixel_position_x=(x+0.5)/nx;
+         pixel_position_x=(y+0.5)/ny;
+rannum_rayOriginXcoord_onAperture和rannum_rayOriginYcoord_onAperture是(0,1)的随机数,
+用来在以center为圆心,lens_radius为半径的照相机光圈上生成随机的光线起始点origin.
+*/
+Ray Camera::getRay(float pixel_position_x, float pixel_position_y, float rannum_rayOriginXcoord_onAperture,
+	float rannum_rayOriginYcoord_onAperture)
+{
+	Vector3 origin = center + 2.0f*(rannum_rayOriginXcoord_onAperture - 0.5f)*lens_radius*uvw.u()
+		+ 2.0f*(rannum_rayOriginYcoord_onAperture - 0.5f)*lens_radius*uvw.v();
+	Vector3 target = image_plane_corner + pixel_position_x*image_plane_uVector + pixel_position_y*image_plane_vVector;
+	/*
+	注意这里计算像素点的世界坐标时,是用:成像平面的左下角坐标(已经转化为世界坐标)+(像素相对于成像平面坐标系的坐标),
+	并且注意到成像平面坐标系的坐标轴分量image_plane_uVector=(u1-u0)*uvw.u()和image_plane_vVector=(v1-v0)*uvw.v()
+	其实已经转化为照相机的坐标轴分量了,而照相机的坐标是相对于世界坐标系的,所以这里的target(目标像素)的坐标
+	其实也是在世界坐标系里的.
+	*/
+	return Ray(origin, unitVector(target - origin));
+}
--- RayTracing/Camera.h
+++ RayTracing/Camera.h
@@ -2,9 +2,24 @@
 #define CAMERA_H
 #include"ONB.h"
 #include"Vector3.h"
+#include"Ray.h"
 class Camera
 {
-
+private:
+	Vector3 center;//照相机光圈中心
+	Vector3 image_plane_corner;//成像矩形平面的左下角
+	Vector3 image_plane_uVector;//成像矩形平面的u向量
+	Vector3 image_plane_vVector;//成像矩形平面的v向量
+	ONB uvw;//照相机坐标系
+	float lens_radius;//照相机光圈半径
+	float u0,v0,u1,v1;//成像矩形平面的左下角坐标(u0,v0)和右上角坐标(u1,v1),
+	float distance;//照相机光圈中心与成像平面的距离
+public:
+	Camera(Vector3 c,Vector3 gaze,Vector3 vup,float apertureDiameter,
+	float left,float right,float top,float bottom,float distance);
+	Camera(const Camera& cam);
+	Ray getRay(float pixel_position_x, float pixel_position_y, float rannum_rayOriginXcoord_onAperture,
+		float rannum_rayOriginYcoord_onAperture);
 };
 #endif // !CAMERA_H
 
